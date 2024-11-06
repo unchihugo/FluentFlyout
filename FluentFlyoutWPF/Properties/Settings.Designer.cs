58e23a58545f375f620ce8267f9986b571200fd8
Hugo <hu8go8li@gmail.com>
2024-11-06 16:23:29 +0000

    Add setting to enable/disable media player flyout feature

--- FluentFlyoutWPF/Properties/Settings.Designer.cs
+++ FluentFlyoutWPF/Properties/Settings.Designer.cs
@@ -202,5 +202,17 @@ namespace FluentFlyout.Properties {
                 this["LockKeysDuration"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool MediaFlyoutEnabled {
+            get {
+                return ((bool)(this["MediaFlyoutEnabled"]));
+            }
+            set {
+                this["MediaFlyoutEnabled"] = value;
+            }
+        }
     }
 }
