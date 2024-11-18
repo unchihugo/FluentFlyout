0df797f48210da0b33f020c7074b237dc9aaf9f6
Hugo <hu8go8li@gmail.com>
2024-11-18 15:05:03 +0000

    Add LastKnownVersion setting and SettingsManager class
    Introduce LastKnownVersion setting in App.config, Settings.Designer.cs,
    and Settings.settings to store the last known version of the application.
    Add SettingsManager class in SettingsManager.cs to manage application
    settings with methods to check, update, restore, and save settings.

---
+++ FluentFlyoutWPF/Classes/SettingsManager.cs
@@ -0,0 +1,94 @@
+﻿using FluentFlyout.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.ApplicationModel;
+
+namespace FluentFlyout.Classes
+{
+    class SettingsManager
+    {
+        private static string SettingsFilePath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "FluentFlyout",
+            "settings.json"
+        );
+
+        /// <summary>
+        /// Checks whether the app has updated, and restores the settings from the previous version if necessary. Only updates in release mode.
+        /// </summary>
+        public static void CheckAndUpdateSettings()
+        {
+            try // gets the version of the app, works only in release mode
+            {
+                var version = Package.Current.Id.Version;
+                string versionString = $"{version.Major}.{version.Minor}.{version.Build}";
+                // if local settings file's version number is different from the current version, update the settings
+                if (!File.Exists(SettingsFilePath) || Settings.Default.LastKnownVersion != versionString) return;
+                else
+                {
+                    RestoreSettings();
+                }
+            }
+            catch
+            {
+                // don't update settings if version is not available
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Restores the app settings from the settings file.
+        /// </summary>
+        private static void RestoreSettings()
+        {
+            try
+            {
+                if (File.Exists(SettingsFilePath))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
+                    using (FileStream fileStream = new FileStream(SettingsFilePath, FileMode.Open))
+                    {
+                        Settings settingsFile = (Settings)xmlSerializer.Deserialize(fileStream);
+
+                        foreach (System.Configuration.SettingsProperty property in settingsFile.Properties)
+                        {
+                            Settings.Default[property.Name] = settingsFile[property.Name];
+                        }
+                        Settings.Default.Save();
+                    }
+                }
+            }
+            catch
+            {
+                // if settings file is corrupted or cannot be read
+            }
+        }
+
+        /// <summary>
+        /// Saves the app settings to the settings file.
+        /// </summary>
+        public static void SaveToSettingsFile()
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(SettingsFilePath));
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
+                using (FileStream fileStream = new FileStream(SettingsFilePath, FileMode.Create))
+                {
+                    xmlSerializer.Serialize(fileStream, Settings.Default);
+                }
+            }
+            catch
+            {
+                // could not save settings to file
+            }
+        }
+    }
+}
--- FluentFlyoutWPF/Properties/Settings.Designer.cs
+++ FluentFlyoutWPF/Properties/Settings.Designer.cs
@@ -262,5 +262,17 @@ namespace FluentFlyout.Properties {
                 this["LockKeysBoldUI"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastKnownVersion {
+            get {
+                return ((string)(this["LastKnownVersion"]));
+            }
+            set {
+                this["LastKnownVersion"] = value;
+            }
+        }
     }
 }
