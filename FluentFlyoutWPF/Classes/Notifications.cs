1784fd3af3c67cba059209a4fe01cb74f5c7cc83
Hugo <hu8go8li@gmail.com>
2025-12-23 17:44:05 +0100

    Reapply first run and debug notification return

--- FluentFlyoutWPF/Classes/Notifications.cs
+++ FluentFlyoutWPF/Classes/Notifications.cs
@@ -9,7 +9,7 @@ namespace FluentFlyout.Classes;
 internal static class Notifications
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-    
+
     /// <summary>
     /// Handle toast notification activation
     /// </summary>
@@ -44,41 +44,38 @@ internal static class Notifications
     /// <param name="currentVersion"></param>
     public static void ShowFirstOrUpdateNotification(string lastKnownVersion, string currentVersion)
     {
-        //if (String.IsNullOrEmpty(lastKnownVersion))
-        //{
-        //    // first run
-        //    return;
+        if (String.IsNullOrEmpty(lastKnownVersion))
+        {
+            // first run
+            return;
 
-        //    new ToastContentBuilder()
-        //        .AddAppLogoOverride(new Uri("ms-appx:///Assets/FluentFlyoutLogo.png"), ToastGenericAppLogoCrop.None)
-        //        .AddText("Welcome to FluentFlyout!")
-        //        .AddText("Thank you for installing FluentFlyout. You can access the settings at any time by clicking the tray icon.")
-        //        .Show();
+            //new ToastContentBuilder()
+            //    .AddText("Welcome to FluentFlyout!")
+            //    .AddText("Thank you for installing FluentFlyout. You can access the settings at any time by clicking the tray icon.")
+            //    .Show();
 
-        //    //return;
-        //}
+            //return;
+        }
 
-        //if (currentVersion == "debug")
-        //{
-        //    return;
+        if (currentVersion == "debug" || lastKnownVersion == "debug")
+        {
+            // debug build or when switching between debug and release builds
+            return;
 
-        //    new ToastContentBuilder()
-        //        .AddAppLogoOverride(new Uri("file:///" + Directory.GetCurrentDirectory() + "/Resources/FluentFlyout2.ico"), ToastGenericAppLogoCrop.None)
-        //        .AddText("FluentFlyout Debug Build")
-        //        .AddText("You are running a debug build.")
-        //        .Show();
+            //new ToastContentBuilder()
+            //    .AddText("FluentFlyout Debug Build")
+            //    .AddText("You are running a debug build.")
+            //    .Show();
 
-        //    return;
-        //}
+            //return;
+        }
 
         if (lastKnownVersion != currentVersion)
         {
             try
             {
-
-                // updated app
+                // updated app version
                 new ToastContentBuilder()
-                    //.AddAppLogoOverride(new Uri("file:///" + Directory.GetCurrentDirectory() + "/Resources/FluentFlyout2.ico"), ToastGenericAppLogoCrop.None
                     .AddText("FluentFlyout has been updated!")
                     .AddText($"You are now on {currentVersion}.")
                     .AddButton(new ToastButton()
