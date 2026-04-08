ac6789d6c38d697403df5fdf1028e1b0822c21a3
Galagyy <109639693+Galagyy@users.noreply.github.com>
2026-04-07 18:23:44 -0700

    refactor: remove disk cache & icons from lists
    - Revert `MediaPlayerData` to use previous implementation
    - Remove `AppNameIconConverter` as it's no longer needed
    - Remove app icons from the whitelist/blacklist UI

--- FluentFlyoutWPF/Classes/Utils/MediaPlayerData.cs
+++ FluentFlyoutWPF/Classes/Utils/MediaPlayerData.cs
@@ -121,68 +121,6 @@ public static class MediaPlayerData
             Icon = mediaIcon
         };
 
-        if (mediaIcon != null)
-        {
-            // Cached against both for consistency.
-            SaveIconToDisk(mediaTitle, mediaIcon);
-            SaveIconToDisk(mediaPlayerId, mediaIcon);
-        }
-
         return (mediaTitle, mediaIcon);
-    }
-
-    /// <summary>
-    /// Saves the specified application icon to disk as a PNG file in the user's application data directory. (Usually )
-    /// </summary>
-    /// <remarks>If the icon is not a BitmapSource, the method does not perform any operation. The icon is
-    /// saved in a subdirectory named 'FluentFlyout\AppIcons' under the user's application data folder. If a file with the same name exists,
-    /// it will be overriden.</remarks>
-    /// <param name="appName">The name of the application. Used to generate the file name for the saved icon. Cannot contain invalid file name
-    /// characters.</param>
-    /// <param name="icon">The icon to save. Must be an ImageSource that is a BitmapSource, otherwise, the method does nothing.</param>
-    public static void SaveIconToDisk(string appName, ImageSource icon)
-    {
-        if (icon is not BitmapSource bitmapSource) return;
-
-        try
-        {
-            var cacheDir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FluentFlyout", "AppIcons");
-            System.IO.Directory.CreateDirectory(cacheDir);
-
-            var safeAppName = string.Join("_", appName.Split(System.IO.Path.GetInvalidFileNameChars()));
-            var filePath = System.IO.Path.Combine(cacheDir, safeAppName + ".png");
-
-            using var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create);
-
-            BitmapEncoder encoder = new PngBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
-            encoder.Save(fileStream);
-        }
-        catch { }
-    }
-
-    public static ImageSource? GetIconFromDisk(string appName)
-    {
-        try
-        {
-            var cacheDir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FluentFlyout", "AppIcons");
-            var safeAppName = string.Join("_", appName.Split(System.IO.Path.GetInvalidFileNameChars()));
-            var filePath = System.IO.Path.Combine(cacheDir, safeAppName + ".png");
-
-            if (System.IO.File.Exists(filePath))
-            {
-                var bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                bitmap.UriSource = new Uri(filePath);
-                bitmap.EndInit();
-                bitmap.Freeze();
-
-                return bitmap;
-            }
-        }
-        catch { }
-
-        return null;
     }
 }
