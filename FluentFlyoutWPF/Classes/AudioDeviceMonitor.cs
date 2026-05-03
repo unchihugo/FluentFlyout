68464fa30baf50b04fa75642f37413ac35f7aa49
Hugo <hu8go8li@gmail.com>
2026-05-03 17:36:29 +0100

    Fix AudioDeviceMonitor changed default device logic

--- FluentFlyoutWPF/Classes/AudioDeviceMonitor.cs
+++ FluentFlyoutWPF/Classes/AudioDeviceMonitor.cs
@@ -2,18 +2,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
 
 namespace FluentFlyoutWPF.Classes
 {
     public class AudioDeviceMonitor : IDisposable
     {
-        // TODO: implement OnDefaultDeviceChanged event - currently gets handled by Visualizer and VolumeMixerViewModel
-
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private static AudioDeviceMonitor? _instance;
         private static readonly object _instanceLock = new();
 
         private MMDeviceEnumerator? _deviceEnumerator;
+        private AudioDeviceNotificationClient? _notificationClient;
 
         public event EventHandler<DefaultDeviceChangedEventArgs>? DefaultDeviceChanged;
 
@@ -42,6 +42,10 @@ namespace FluentFlyoutWPF.Classes
             try
             {
                 _deviceEnumerator = new MMDeviceEnumerator();
+                _notificationClient = new AudioDeviceNotificationClient();
+                _notificationClient.DefaultDeviceChanged += OnDefaultDeviceChanged;
+                _deviceEnumerator.RegisterEndpointNotificationCallback(_notificationClient);
+
                 Logger.Info("Audio device monitoring initialized");
             }
             catch (Exception ex)
@@ -50,11 +54,24 @@ namespace FluentFlyoutWPF.Classes
             }
         }
 
+        private void OnDefaultDeviceChanged(object? sender, DefaultDeviceChangedEventArgs e)
+        {
+            // Windows fires this for every Role (Console, Multimedia, Communications).
+            // Only forward Render/Multimedia to avoid triple-firing on a single switch.
+            if (e.DataFlow != DataFlow.Render)
+                return;
+
+            Logger.Info("Default audio output device changed");
+            DefaultDeviceChanged?.Invoke(this, e);
+        }
+
         public MMDevice? GetDefaultRenderDevice()
         {
             try
             {
-                return _deviceEnumerator?.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                var device = _deviceEnumerator?.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                Logger.Debug("New device: {0}", device);
+                return device;
             }
             catch (Exception ex)
             {
@@ -63,24 +80,64 @@ namespace FluentFlyoutWPF.Classes
             }
         }
 
+        public MMDevice? GetDeviceById(string deviceId)
+        {
+            try
+            {
+                var device = _deviceEnumerator?.GetDevice(deviceId);
+                Logger.Debug("Got device by id {0}: {1}", deviceId, device);
+                return device;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Failed to get device by id {0}", deviceId);
+                return null;
+            }
+        }
+
         public void Dispose()
         {
-            _deviceEnumerator?.Dispose();
+            _notificationClient?.DefaultDeviceChanged -= OnDefaultDeviceChanged;
+
+            if (_deviceEnumerator != null && _notificationClient != null)
+            {
+                try
+                {
+                    _deviceEnumerator.UnregisterEndpointNotificationCallback(_notificationClient);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Failed to unregister device notification callback");
+                }
+                _deviceEnumerator = null;
+            }
+
+            _notificationClient = null;
+
             GC.SuppressFinalize(this);
         }
     }
 
-    public class DefaultDeviceChangedEventArgs : EventArgs
+    // classes to handle audio device notifications
+    public class AudioDeviceNotificationClient : IMMNotificationClient
     {
-        public DataFlow DataFlow { get; }
-        public Role Role { get; }
-        public string DeviceId { get; }
+        public event EventHandler<DefaultDeviceChangedEventArgs>? DefaultDeviceChanged;
 
-        public DefaultDeviceChangedEventArgs(DataFlow dataFlow, Role role, string deviceId)
+        public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
         {
-            DataFlow = dataFlow;
-            Role = role;
-            DeviceId = deviceId;
+            DefaultDeviceChanged?.Invoke(this, new DefaultDeviceChangedEventArgs(flow, role, defaultDeviceId));
         }
+
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState) { }
+        public void OnDeviceAdded(string pwstrDeviceId) { }
+        public void OnDeviceRemoved(string deviceId) { }
+        public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) { }
+    }
+
+    public class DefaultDeviceChangedEventArgs(DataFlow dataFlow, Role role, string deviceId) : EventArgs
+    {
+        public DataFlow DataFlow { get; } = dataFlow;
+        public Role Role { get; } = role;
+        public string DeviceId { get; } = deviceId;
     }
 }
